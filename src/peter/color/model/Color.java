89d681dda5e2166547f40a2bd2fc651e3026ce40
Clemence Tafforeau <clem.tafforeau@gmail.com>
2026-02-16 10:27:03 +0100

    chore(init): setting up the project

---
+++ src/peter/color/model/Color.java
@@ -0,0 +1,48 @@
+package peter.color.model;
+
+public class Color {
+
+    public Color(int red, int green, int blue) {
+        //
+    }
+
+    public Color(String hexValue) {
+        //
+    }
+
+    public int getRed() {
+        return 0;
+    }
+
+    public void setRed() {
+        //
+    }
+
+    public int getGreen() {
+        return 0;
+    }
+
+    public void setGreen() {
+        //
+    }
+
+    public int getBlue() {
+        return 0;
+    }
+
+    public void setBlue() {
+        //
+    }
+
+    public int getHexValue() {
+        return 0;
+    }
+
+    public void setHexValue() {
+        //
+    }
+
+    public String toString() {
+        return "";
+    }
+}
